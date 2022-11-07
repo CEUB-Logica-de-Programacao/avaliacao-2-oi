1709d915b4ca3bf25217123f7e7ca444b300c955
Lucas Moura <111282835+lucpes@users.noreply.github.com>
2022-11-07 14:27:02 -0300

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -18,9 +18,41 @@
 
 
 def bonus(nums):
-    # Escreva seu código aqui
-    pass
+    lista_len = []
 
+    contador = 9
+    while contador != -1:
+        lista_provisoria = []
+        for num in nums:
+            if str(num)[0] == str(contador):
+                lista_provisoria.append(str(num))
+
+        if lista_provisoria:
+            lista_provisoria.sort(reverse=True)
+            lista_len.append(lista_provisoria)
+        contador -= 1
+
+    for lista_num in lista_len:
+        for num in lista_num:
+            if str(num)[len(str(num)) - 1] == '0':
+                lista_num.remove(num)
+                lista_num.append(num)
+
+    lista_len_str = []
+    total = ""
+    for lista_num in lista_len:
+        for num in lista_num:
+            lista_len_str.append(str(num))
+
+    for termo in lista_len_str:
+        total += termo
+    lista_len_int = []
+    for n in total:
+        lista_len_int.append(int(n))
+    if sum(lista_len_int) == 0:
+        return '0'
+    print(total)
+    return total
 
 if __name__ == '__main__':
     print(bonus([3, 30, 34, 5, 9]))
